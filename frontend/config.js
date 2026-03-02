f5ca46a5ac851c68676f6374845254a71a92274f
lyaminedb1 <102860167+lyaminedb1@users.noreply.github.com>
2026-03-02 17:28:40 +0100

    Update API URL for production

--- frontend/config.js
+++ frontend/config.js
@@ -3,5 +3,5 @@
  * ⚠️  STUDENTS: replace the URL below with your own Render service URL.
  */
 const CONFIG = {
-    API_URL: "https://fraud-detection-api-haer.onrender.com",
+    API_URL: "https://api.render.com/deploy/srv-d6iqj4p4tr6s73cjjd1g?key=JiMaNjfvztQ",
 };
